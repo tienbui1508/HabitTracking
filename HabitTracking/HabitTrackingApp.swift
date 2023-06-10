d4bf7fe7f2aa6e27dee572fae91036f01f470680
tienbui <tienbui1508@gmail.com>
2023-06-11 01:34:21 +0930

    Initial Commit

---
+++ HabitTracking/HabitTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackingApp.swift
+//  HabitTracking
+//
+//  Created by Tien Bui on 11/6/2023.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
